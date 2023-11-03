2ed7c76d8b928eabdb391b278d933b90939c9431
samuel-eric <samueltan086@gmail.com>
2023-11-03 11:27:38 +0700

    Add submission for exercise 3.11

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const app = express();
 
 morgan.token('data', (request, response) => JSON.stringify(request.body));
 
+app.use(express.static('dist'));
 app.use(express.json());
 app.use(
 	morgan(':method :url :status :res[content-length] - :response-time ms :data')
