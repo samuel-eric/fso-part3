501f7e0e235a6c53f0a059a829071550c2cab173
samuel-eric <samueltan086@gmail.com>
2023-11-03 11:09:08 +0700

    Add submission for exercise 3.9

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const morgan = require('morgan');
+const cors = require('cors');
 const app = express();
 
 morgan.token('data', (request, response) => JSON.stringify(request.body));
@@ -8,6 +9,7 @@ app.use(express.json());
 app.use(
 	morgan(':method :url :status :res[content-length] - :response-time ms :data')
 );
+app.use(cors());
 
 let persons = [
 	{
