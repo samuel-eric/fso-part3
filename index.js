c83a195966658a9de83ea5cbcc4187887bbd8984
samuel-eric <samueltan086@gmail.com>
2023-11-03 07:58:43 +0700

    Add submission for exercise 3.5

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 
+app.use(express.json());
+
 let persons = [
 	{
 		id: 1,
@@ -50,6 +52,19 @@ app.delete('/api/persons/:id', (request, response) => {
 	const id = Number(request.params.id);
 	persons = persons.filter((person) => person.id !== id);
 	response.status(204).end();
+});
+
+const generateId = () => Math.floor(Math.random() * 100000000);
+
+app.post('/api/persons', (request, response) => {
+	let id = generateId();
+	const duplicateId = persons.find((person) => person.id === id);
+	while (duplicateId) {
+		id = generateId;
+	}
+	const person = { id, ...request.body };
+	persons = persons.concat(person);
+	response.json(person);
 });
 
 const PORT = 3001;
